53bae68c81ecd99149986d337e743559cc871e0d
Harshita1336 <harshita1336.becse24@chitkara.edu.in>
2025-03-29 00:49:14 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -22,7 +22,7 @@ int main(){
 	int a[n];
 	int i;
 	for (i=0;i<n;i++)
-	{scanf("%d ", &a[i]);   // extra space after %d was the mistake 
+	{scanf("%d", &a[i]);   // extra space after %d was the mistake 
 	}
 	for (i=n-1;i>=0;i--){
 		printf("%d /n", a[i]);
